6ea370e8cdfc1400e5c06f23969b55b3af4d9121
Povilasss <povilas.nanartonis.pn@gmail.com>
2024-04-24 20:40:14 +0300

    Rodomas prisijungusio vartotojo El.pastas

--- app/src/main/java/com/example/keiskisdaiktais/Pagrindinis.java
+++ app/src/main/java/com/example/keiskisdaiktais/Pagrindinis.java
@@ -1,6 +1,7 @@
 package com.example.keiskisdaiktais;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Pagrindinis extends AppCompatActivity {
+
+    private TextView textViewUserEmail;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_pagrindinis);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        mAuth = FirebaseAuth.getInstance();
+
+        textViewUserEmail = findViewById(R.id.textViewUserEmail);
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser != null) {
+            textViewUserEmail.setText("Vartotojas: " + currentUser.getEmail());
+        }
+
     }
 }
